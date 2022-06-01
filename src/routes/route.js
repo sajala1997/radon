5c7826d948a58a1dd11d4dbd604bc32a58a47faa
sajala soni <sajala.soni1997@gmail.com>
2022-06-01 21:26:16 +0530

    routes

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const externalModule1 = require('../logger/logger')
+const externalModule3 = require('../util/helper')
+const externalModule2 = require('../validator/formatter')
 
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
+    externalModule1.welcome()
+    externalModule2.trim()
+    externalModule2.changetoLowerCase()
+    externalModule2.changetoUpperCase()
+    externalModule3.printDate()
+    externalModule3.printMonth()
+    externalModule3.getBatchInfo()
     res.send('My first ever api!')
 });
 
