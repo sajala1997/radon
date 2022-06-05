177cd3e4621cdfcbf666f422e64365d4f7174be0
sajala soni <sajala.soni1997@gmail.com>
2022-06-05 18:29:16 +0530

    module package done

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,12 +3,17 @@ const router = express.Router();
 const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+// router.get("/test-me", function (req, res) {
+//     res.send("My first ever api!")
+// })
 
-router.post("/createUser", UserController.createUser  )
 
-router.get("/getUsersData", UserController.getUsersData)
+router.post("/createBook", UserController.createBook )
+
+router.get("/getBookData", UserController.getBookData)
+
+// router.post("/createUser", UserController.createUser  )
+
+// router.get("/getUsersData", UserController.getUsersData)
 
 module.exports = router;
