68aeafb58896b3f96702114d1f2965a5465ed8d8
sajala soni <sajala.soni1997@gmail.com>
2022-06-07 10:32:37 +0530

    book

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Sajala1997:wbcDbgodbFCDVNuz@cluster0.yjgci.mongodb.net/Sajala1997-DB", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,31 +1,28 @@
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
+    bookName: {
+        type :String, 
+        required :true
+        },
     
-    isPublished: Boolean,
+  
     prices: {
         indianPrice: String,
         europePrice: String,
     },
-    sales: {type: Number, default: 10}
+    year : {type: Number,
+        default: 2021
+            },
+    authorName: String, 
+    tags: [String],
+    totalPages : Number,
+    stockAvailable : Boolean
+
+    
 }, { timestamps: true });
 
 
-module.exports = mongoose.model('Book', bookSchema) //users
+module.exports = mongoose.model('Book', bookSchema) //books
 
-//Validation:
-//require:true
-//unique
-// default
 
-//String
-//Number
-//Date
-//Boolean
-// Arrays
-// Object
-// ObjectId
-// Buffer - not cover
